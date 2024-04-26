5555d29ac6fb503d6554836694c43c3bc8f789cd
Daria <daria361987@gmail.com>
2024-04-27 00:42:16 +0200

    added task_score.js auto 2.1.3

---
+++ task_score.js
@@ -0,0 +1,11 @@
+let introScore = 50;
+let gitScore = 78;
+let jsScore = 86;
+
+let averageScore = (introScore + gitScore + jsScore) / 3;
+
+const studentName = "Ivan";
+const studentSurname = "Ivanov";
+const fullName = studentName + " " + studentSurname;
+
+console.log(`${fullName}'s average score is ${averageScore}`);
