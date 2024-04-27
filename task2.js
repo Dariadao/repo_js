5c28813fc7f87e010137c2440ad6a34d83ff7a47
Daria <daria361987@gmail.com>
2024-04-27 17:27:08 +0200

    added task2 hw2.1.5

---
+++ task2.js
@@ -0,0 +1,10 @@
+const value1 = 50;
+let value2 = Math.floor(Math.random() * 100);
+
+let result = value1 + value2 / 5 + 17;
+console.log(value2, result);
+
+/* Приоритет операторов в JS, как в математике.
+Сначала выполняются действия в скобках,
+затем деление и умножение,
+далее сложение и вычитание. */
--- task_score.js
+++ task_score.js
@@ -8,4 +8,9 @@ const studentName = "Ivan";
 const studentSurname = "Ivanov";
 const fullName = studentName + " " + studentSurname;
 
-console.log(`${fullName}'s average score is ${averageScore}`);
+console.log(
+  `${studentName.slice(
+    0,
+    1
+  )}. ${studentSurname}'s average score is ${averageScore}`
+);
