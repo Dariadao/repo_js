141b60cd59cde645ec7de7dd96fcbb7944943e94
Daria <daria361987@gmail.com>
2024-05-05 18:00:52 +0200

    modified task 2_3_1_2

--- task_2_3_1_1.js
+++ task_2_3_1_1.js
@@ -21,25 +21,115 @@ const players = [
   },
 ];
 
+const testPlayers1 = [
+  {
+    id: 1,
+    name: "John",
+    scorePoints: 1,
+  },
+  {
+    id: 2,
+    name: "Alice",
+    scorePoints: -1,
+  },
+  {
+    id: 3,
+    name: "Bob",
+    scorePoints: 0,
+  },
+  // Ожидаемый результат: { id: 1, name: "John", scorePoints: 5000 }
+];
+
+const testPlayers2 = [
+  {
+    id: 1,
+    name: "Emily",
+    scorePoints: 3500.01,
+  },
+  {
+    id: 2,
+    name: "David",
+    scorePoints: 3499.99,
+  },
+  {
+    id: 3,
+    name: "Sophia",
+    scorePoints: 3500,
+  },
+];
+// Ожидаемый результат:{ id: 1, name: "Emily", scorePoints: 3500 }
+
+const testPlayers3 = [];
+// Ожидаемый результат: undefined (так как массив пустой)
+
+const testPlayers4 = [
+  {
+    id: 1,
+    name: "Mark",
+    scorePoints: -2000,
+  },
+  {
+    id: 2,
+    name: "Emma",
+    scorePoints: -1500,
+  },
+  {
+    id: 3,
+    name: "Liam",
+    scorePoints: -3000,
+  },
+];
+// Ожидаемый результат: { id: 2, name: 'Emma', scorePoints: -1500 }
+
+const testPlayers5 = [
+  {
+    id: 1,
+    name: "Olivia",
+    scorePoints: 2500,
+  },
+  {
+    id: 2,
+    name: "Noah",
+    scorePoints: 2500,
+  },
+  {
+    id: 3,
+    name: "Ava",
+    scorePoints: 2500,
+  },
+];
+// Ожидаемый результат: любой из игроков с одинаковым максимальным количеством очков
+
 let scores = [];
 
-for (let player of players) {
-  for (let key in player) {
-    if (key === "scorePoints") {
-      scores.push(player[key]);
+function findPlayerWithMaxScore(testData) {
+  scores = [];
+
+  for (let player of testData) {
+    for (let key in player) {
+      if (key === "scorePoints") {
+        scores.push(player[key]);
+      }
     }
   }
-}
-console.log(scores);
+  console.log(scores);
 
-const maxScore = Math.max(...scores);
-console.log(maxScore);
+  let maxScore = Math.max(...scores);
+  console.log(maxScore);
 
-let maxScorePlayer;
-for (let player of players) {
-  if (player.scorePoints === maxScore) {
-    maxScorePlayer = player;
-    break;
+  let maxScorePlayer;
+  for (let player of testData) {
+    if (player.scorePoints === maxScore) {
+      maxScorePlayer = player;
+      break;
+    }
   }
+  return maxScorePlayer;
 }
-console.log(maxScorePlayer);
+
+console.log(findPlayerWithMaxScore(players));
+console.log(findPlayerWithMaxScore(testPlayers1));
+console.log(findPlayerWithMaxScore(testPlayers2));
+console.log(findPlayerWithMaxScore(testPlayers3));
+console.log(findPlayerWithMaxScore(testPlayers4));
+console.log(findPlayerWithMaxScore(testPlayers5));
