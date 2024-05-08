51503742e4550b16296cd0a3d9a9139b023691df
Daria <daria361987@gmail.com>
2024-05-08 23:33:26 +0200

    added  task_2_4_1.js

---
+++ task_2_4_1.js
@@ -0,0 +1,38 @@
+let expencesExamples = [
+  {
+    yearlyExpences: [
+      2500, 6000, 7000, 6000, 3, 9, 700, 8000, 655, 5700, 340, 5390,
+    ],
+  },
+  {
+    yearlyExpences: [
+      500, 1000, 90000, 80, 980, 9000, 10, 400, 3000, 250, 45000, 1200,
+    ],
+  },
+  {
+    yearlyExpences: [
+      20, 200, 8900, 3300, 2300, 1009, 1200, 2000, 670, 900, 900, 7000,
+    ],
+  },
+];
+
+let yearlyExpences = [
+  400, 1050, 700, 800, 550, 2800, 1100, 850, 2000, 900, 1200, 780,
+];
+
+function calculateSumOfYearlyExpences(expences) {
+  let sum = 0;
+  for (let i = 0; i < expences.length; i++) {
+    if (expences[i] > 1000) sum += expences[i];
+  }
+  console.log(sum);
+  return sum;
+}
+
+calculateSumOfYearlyExpences(yearlyExpences);
+
+expencesExamples.forEach((example) => {
+  let yearlyExpencesObj = example.yearlyExpences;
+  let sumObj = calculateSumOfYearlyExpences(yearlyExpencesObj);
+  console.log(sumObj);
+});
